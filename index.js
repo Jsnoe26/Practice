0a961ec8dd961d117e16c281fba31289237af709
SnoeProductions <>
2021-05-21 03:46:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let bobbingForApples = "10";
+let ringToss = "10";
+console.log(bobbingForApples);
+if (ringToss = "10"); {
+  console.log("Oops! Try Again!");
+}
+if (bobbingForApples = "10"); {
+  console.log("Winner! Collect your PRIZE!");
+}
